babdf66b6295acb19893cb7ee0fe540cb1e7d185
nhatthanh2012 <nhatthanh2012@gmail.com>
2019-06-03 08:55:32 +0700

    >

---
+++ Module 2/shop/resources/views/footer.blade.php
@@ -0,0 +1,62 @@
+<div id="footer" class="color-div">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="widget">
+						<h4 class="widget-title">Instagram Feed</h4>
+						<div id="beta-instagram-feed"><div></div></div>
+					</div>
+				</div>
+				<div class="col-sm-2">
+					<div class="widget">
+						<h4 class="widget-title">Information</h4>
+						<div>
+							<ul>
+								<li><a href="http://ct14a1.890m.com/"><i class="fa fa-chevron-right"></i> Web Design</a></li>
+								<li><a href="http://ct14a1.890m.com/"><i class="fa fa-chevron-right"></i> Web development</a></li>
+								<li><a href="http://ct14a1.890m.com/"><i class="fa fa-chevron-right"></i> Marketing</a></li>
+								<li><a href="http://ct14a1.890m.com/"><i class="fa fa-chevron-right"></i> Tips</a></li>
+								<li><a href="http://ct14a1.890m.com/"><i class="fa fa-chevron-right"></i> Resources</a></li>
+								<li><a href="http://ct14a1.890m.com/"><i class="fa fa-chevron-right"></i> Illustrations</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-4">
+				 <div class="col-sm-10">
+					<div class="widget">
+						<h4 class="widget-title">Contact Us</h4>
+						<div>
+							<div class="contact-info">
+								<i class="fa fa-map-marker"></i>
+								<p><a href="{{route('lienhe')}}">Location in the Map</a></p>
+								<p>33 Xô Viết Nghệ Tĩnh - Đà Nẵng Phone: 01.648.638.658</p>
+							</div>
+						</div>
+					</div>
+				  </div>
+				</div>
+				<div class="col-sm-3">
+					<div class="widget">
+						<h4 class="widget-title">Newsletter Subscribe</h4>
+						<form action="#" method="post">
+							<input type="email" name="your_email">
+							<button class="pull-right" type="submit">Subscribe <i class="fa fa-chevron-right"></i></button>
+						</form>
+					</div>
+				</div>
+			</div> <!-- .row -->
+		</div> <!-- .container -->
+	</div> <!-- #footer -->
+	<div class="copyright">
+		<div class="container">
+			<p class="pull-left">Shop bánh online | Bánh kẹo online | . (&copy;) 2017 </p>
+			<p class="pull-right pay-options">
+				<a href=" http://facebook.com/DuyRNT" target="_blank"><img style="width: 30px;height: 30px;line-height: 30px;margin-top: 10px;" src="source/image/facebook-256.png" alt="" /></a>
+				<a href=" http://twitter.com/Duy_RNT" target="_blank"><img style="width: 30px;height: 30px;line-height: 30px;margin-top: 10px;" src="source/image/twitter-icon-9.png" alt="" /></a>
+				<a href="https://plus.google.com/101476865900745011574" target="_blank"><img style="width: 30px;height: 30px;line-height: 30px;margin-top: 10px;" src="source/image/7-128.png" alt="" /></a>
+				<a href="https://www.youtube.com/channel/UCyNzr2FnPyf3E2H7QefgMww" target="_blank"><img style="width: 30px;height: 30px;line-height: 30px;margin-top: 10px;" src="source/image/YouTube-icon-full_color.png" alt="" /></a>
+			</p>
+			<div class="clearfix"></div>
+		</div> <!-- .container -->
+	</div> <!-- .copyright -->
---
+++ Module 2/shop/resources/views/page/lienhe.blade.php
@@ -0,0 +1,76 @@
+@extends('master')
+@section('content')
+	<div class="inner-header">
+		<div class="container">
+			<div class="pull-left">
+				<h6 class="inner-title">Contacts</h6>
+			</div>
+			<div class="pull-right">
+				<div class="beta-breadcrumb font-large">
+					<a href="index">Home</a> / <span>Contacts</span>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<div class="beta-map">
+		
+		<div class="abs-fullwidth beta-map wow flipInX"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3834.641387766022!2d108.21434631441737!3d16.032172988903838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314219ed0c184013%3A0x80f3ad626cd8aea6!2zWMO0IFZp4bq_dCBOZ2jhu4cgVMSpbmgsIMSQw6AgTuG6tW5nLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1491448283880" width="600" height="450" frameborder="0" style="border:0" allowfullscreen" ></iframe></div>
+	</div>
+	<div class="container">
+		<div id="content" class="space-top-none">
+			
+			<div class="space50">&nbsp;</div>
+			<div class="row">
+				<div class="col-sm-8">
+					<h2>Contact Form</h2>
+					<div class="space20">&nbsp;</div>
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit ani m id est laborum.</p>
+					<div class="space20">&nbsp;</div>
+					<form action="#" method="post" class="contact-form">	
+						<div class="form-block">
+							<input name="your-name" type="text" placeholder="Your Name (required)">
+						</div>
+						<div class="form-block">
+							<input name="your-email" type="email" placeholder="Your Email (required)">
+						</div>
+						<div class="form-block">
+							<input name="your-subject" type="text" placeholder="Subject">
+						</div>
+						<div class="form-block">
+							<textarea name="your-message" placeholder="Your Message"></textarea>
+						</div>
+						<div class="form-block">
+							<button type="submit" class="beta-btn primary">Send Message <i class="fa fa-chevron-right"></i></button>
+						</div>
+					</form>
+				</div>
+				<div class="col-sm-4">
+					<h2>Contact Information</h2>
+					<div class="space20">&nbsp;</div>
+
+					<h6 class="contact-title">Address</h6>
+					<p>
+						33 Xo Viet Nghe Tinh St,<br>
+						Hai  Chau, Da Nang <br>
+						Viet Nam
+					</p>
+					<div class="space20">&nbsp;</div>
+					<h6 class="contact-title">Business Enquiries</h6>
+					<p>
+						Doloremque laudantium, totam rem aperiam, <br>
+						inventore veritatio beatae. <br>
+						<a href="mailto:biz@betadesign.com">biz@betadesign.com</a>
+					</p>
+					<div class="space20">&nbsp;</div>
+					<h6 class="contact-title">Employment</h6>
+					<p>
+						We’re always looking for talented persons to <br>
+						join our team. <br>
+						<a href="hr@betadesign.com">hr@betadesign.com</a>
+					</p>
+				</div>
+			</div>
+		</div> <!-- #content -->
+	</div> <!-- .container -->
+@endsection
